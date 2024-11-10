cef80fba3da164fd7309cd0334214be9161ba310
박병훈 <oo009pbh@gmail.com>
2024-11-10 20:28:39 +0900

    feature: Header 컴포넌트 생성 진행중

--- app/_shared/components/header/index.tsx
+++ app/_shared/components/header/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import './header.scss';
 
 function Header() {
-  return <div></div>;
+  return (
+    <header className='container'>
+      <div className='header_item'>소개</div>
+      <div className='header_item'>민박</div>
+      <div className='header_item'>qna</div>
+    </header>
+  );
 }
 
 export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import klass from "./main.module.scss"
+import klass from './main.module.scss';
+import Header from '@components/header';
 
 export default function Home() {
   return (
-    <div
-      className={klass.container}>
-      <main>
-        테스트
-      </main>
+    <div className={klass.container}>
+      <Header />
+      <main>테스트</main>
     </div>
   );
 }
