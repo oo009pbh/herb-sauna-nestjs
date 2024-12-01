2a7b3c9ae1d8580839d56497876c1d5cf9cbd901
박병훈 <oo009pbh@gmail.com>
2024-12-01 22:53:09 +0900

    feat(sauna): Faq 생성

--- app/qna/page.tsx
+++ app/qna/page.tsx
@@ -1,9 +1,22 @@
 import klass from './qna.module.scss';
+import Title from '@atoms/title';
+import Faq from '@atoms/faq';
+import { FAQS } from '@/app/_shared/const';
 
-export default function Home() {
+export default function Qna() {
   return (
     <main className={klass.container}>
-      <section className={klass.section1}></section>
+      <Title title={'자주 묻는 질문'} />
+      <div className={klass.faq_container}>
+        {FAQS.map((faq, index) => (
+          <Faq
+            key={index}
+            index={index}
+            title={faq.title}
+            description={faq.description}
+          />
+        ))}
+      </div>
     </main>
   );
 }
