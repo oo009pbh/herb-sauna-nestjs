32372d056aebd9e7e427b1003e78a41549322b18
박병훈 <oo009pbh@gmail.com>
2024-12-01 21:40:25 +0900

    feat(sauna): IntroSection 박스 생성

--- app/_shared/components/templates/IntroSection/index.tsx
+++ app/_shared/components/templates/IntroSection/index.tsx
@@ -5,7 +5,10 @@ import Title from '@atoms/title';
 function IntroSection() {
   return (
     <section className={klass.container}>
-      <Title title={'특별한 점'} />
+      <div className={klass.swiper}>
+        <div className={klass.description}>용문산허브찜질방만의</div>
+        <Title title={'특별한 점'} variant={'white'} />
+      </div>
     </section>
   );
 }
