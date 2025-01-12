2d4acc5d83dc4d257d6eb461c32c77b263c01c9b
박병훈 <oo009pbh@gmail.com>
2025-01-12 18:05:40 +0900

    feat(sauna): viewerjs 연동

--- app/_shared/components/atoms/ImageBox/index.tsx
+++ app/_shared/components/atoms/ImageBox/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useRef } from 'react';
 import klass from './ImageBox.module.scss';
 import Image from 'next/image';
+import Viewer from 'viewerjs';
+import 'viewerjs/dist/viewer.css';
 
 type ImageBoxProps = {
   customKlass?: string;
@@ -13,9 +17,22 @@ function ImageBox({
   src,
   title,
 }: React.PropsWithChildren<ImageBoxProps>) {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const $container = containerRef.current;
+    if ($container) {
+      const $image = $container.querySelector('img');
+
+      if ($image) {
+        new Viewer($image);
+      }
+    }
+  }, []);
+
   return (
-    <div className={`${customKlass} ${klass.container}`}>
-      <Image alt={''} className={klass.bg_left_image} src={src} fill={true} />
+    <div ref={containerRef} className={`${customKlass} ${klass.container}`}>
+      <Image alt={''} src={src} fill={true} />
       <div className={klass.title}>{title}</div>
     </div>
   );
