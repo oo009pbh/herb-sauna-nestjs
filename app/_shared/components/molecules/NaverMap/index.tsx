e802b5a23275f6f82d1c4a189bee3c621d0e0ba7
박병훈 <oo009pbh@gmail.com>
2025-03-06 00:38:29 +0900

    feat(sauna): 약간 수정

--- app/_shared/components/molecules/NaverMap/index.tsx
+++ app/_shared/components/molecules/NaverMap/index.tsx
@@ -13,15 +13,25 @@ function NaverMap({ klass = '' }: React.PropsWithChildren<NaverMapProps>) {
       if (!window.naver) return; // 네이버 객체가 로드되었는지 확인
 
       const SAUNA_LOCATION = {
-        LAT: 37.5286917,
-        LNG: 127.5521031,
+        LAT: 37.5286,
+        LNG: 127.5521,
       };
-      
+
       const mapOptions = {
         center: new naver.maps.LatLng(SAUNA_LOCATION.LAT, SAUNA_LOCATION.LNG), // 서울 중심 좌표
         zoom: 16,
       };
-      new naver.maps.Map('map', mapOptions);
+      const map = new naver.maps.Map('map', mapOptions);
+
+      const marker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(SAUNA_LOCATION.LAT, SAUNA_LOCATION.LNG), // 마커 위치
+        map: map, // 표시할 지도 객체
+        title: '용문산 허브찜질방', // 마커 위에 표시할 텍스트
+      });
+
+      naver.maps.Event.addListener(marker, 'click', function () {
+        alert('마커를 클릭했습니다!');
+      });
     };
 
     // 스크립트 로드 완료 후 실행
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type {NextConfig} from "next";
-import path from "node:path";
+import type { NextConfig } from 'next';
+import path from 'node:path';
 
 const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'app/_styles')],
-    prependData: `@import 'mixins'; @import 'variables';`
+    prependData: `@import 'mixins'; @import 'variables';`,
   },
 };
 
