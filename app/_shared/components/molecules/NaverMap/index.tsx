f22ef7020025867d343bec026285cba5233798f6
박병훈 <oo009pbh@gmail.com>
2025-03-30 18:18:12 +0900

    feat(sauna): 네이버 맵 섹션 수정

--- app/_shared/components/molecules/NaverMap/index.tsx
+++ app/_shared/components/molecules/NaverMap/index.tsx
@@ -1,36 +1,34 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useLayoutEffect } from 'react';
 import Script from 'next/script';
+import klass from './NaverMap.module.scss';
 
-type NaverMapProps = {
-  klass?: string;
-};
-
-function NaverMap({ klass = '' }: React.PropsWithChildren<NaverMapProps>) {
-  useEffect(() => {
+function NaverMap() {
+  useLayoutEffect(() => {
     // 네이버 지도 초기화
     const initMap = () => {
       if (!window.naver) return; // 네이버 객체가 로드되었는지 확인
 
       const SAUNA_LOCATION = {
-        LAT: 37.5286,
-        LNG: 127.5521,
+        LAT: 37.528654,
+        LNG: 127.55213,
       };
 
       const mapOptions = {
-        center: new naver.maps.LatLng(SAUNA_LOCATION.LAT, SAUNA_LOCATION.LNG), // 서울 중심 좌표
+        center: new naver.maps.LatLng(SAUNA_LOCATION.LAT, SAUNA_LOCATION.LNG),
+        zoomControl: true,
         zoom: 16,
       };
       const map = new naver.maps.Map('map', mapOptions);
 
       const marker = new naver.maps.Marker({
-        position: new naver.maps.LatLng(SAUNA_LOCATION.LAT, SAUNA_LOCATION.LNG), // 마커 위치
-        map: map, // 표시할 지도 객체
-        title: '용문산 허브찜질방', // 마커 위에 표시할 텍스트
+        position: new naver.maps.LatLng(SAUNA_LOCATION.LAT, SAUNA_LOCATION.LNG),
+        map: map,
+        title: '용문산 허브찜질방',
       });
 
       naver.maps.Event.addListener(marker, 'click', function () {
-        alert('마커를 클릭했습니다!');
+        window.open('https://naver.me/5N1XCMhc', '_blank');
       });
     };
 
@@ -48,7 +46,7 @@ function NaverMap({ klass = '' }: React.PropsWithChildren<NaverMapProps>) {
       <div
         id='map'
         style={{ width: '100%', height: '500px' }}
-        className={klass}
+        className={klass.container}
       ></div>
 
       <Script
--- app/_shared/components/templates/LocationSection/index.tsx
+++ app/_shared/components/templates/LocationSection/index.tsx
@@ -7,7 +7,25 @@ export function LocationSection() {
   return (
     <section className={klass.container}>
       <Title title={'오시는 길'} />
-      <NaverMap />
+      <div className={klass.content}>
+        <NaverMap />
+        <div className={klass.description_wrapper}>
+          <div className={klass.title}>용문산허브찜질방</div>
+          <div className={klass.description}>
+            <div className={klass.row}>
+              이용시간 : 24시간 운영
+              <br />
+              (일-금 07:00-23:00 / 토 7:00-24:00 입실 가능)
+            </div>
+            <div className={klass.row}>
+              주소 : 경기도 양평군 용문면 태남길 47
+            </div>
+            <div className={klass.row}>
+              연락처 : 031-774-2766 / 010-3227-3240
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
