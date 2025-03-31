6f979e59c921ea9b610db6ed6d8a1bde8c373a1d
ParkByeongHoon <oo009pbh@gmail.com>
2025-03-31 23:54:33 +0900

    민박 잠시 주석처리

--- app/_shared/components/organisms/header/index.tsx
+++ app/_shared/components/organisms/header/index.tsx
@@ -11,9 +11,9 @@ function Header() {
       <Link href={'/intro'} className={klass.header_item}>
         소개
       </Link>
-      <Link href={'/rooms'} className={klass.header_item}>
-        민박
-      </Link>
+      {/*<Link href={'/rooms'} className={klass.header_item}>*/}
+      {/*  민박*/}
+      {/*</Link>*/}
       <Link href={'/qna'} className={klass.header_item}>
         Q&A
       </Link>
