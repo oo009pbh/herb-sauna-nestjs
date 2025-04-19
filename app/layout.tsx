25271ab493b6f06f26006561eb17711e10162584
박병훈 <oo009pbh@gmail.com>
2025-04-19 18:05:41 +0900

    feat(sauna): ico 메타데이터 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,52 @@ export const metadata: Metadata = {
   description:
     '자연 속에 있는 찜질방,\n' +
     '허브스팀으로 향기로운 찜질을 경험을 해보세요.',
+  keywords: [
+    '허브찜질방',
+    '용문산 찜질방',
+    '자연 찜질방',
+    '허브 사우나',
+    '향기 찜질',
+    '스팀 찜질',
+    '힐링 공간',
+    '자연 속 휴식',
+    '양평 찜질방',
+    '용문산 여행',
+    '허브 테라피',
+    '건강 디톡스',
+  ],
+  authors: [{ name: '박병훈' }],
+  robots: 'index, follow',
+  icons: {
+    icon: '/herb_sauna_logo.ico',
+    apple: '/apple-touch-icon.png',
+  },
+  openGraph: {
+    title: '용문산 허브 찜질방',
+    description:
+      '자연 속에 있는 찜질방,\n' +
+      '허브스팀으로 향기로운 찜질을 경험을 해보세요.',
+    url: 'https://herbsauna.co.kr',
+    siteName: '사이트 이름',
+    images: [
+      {
+        url: 'https://herbsauna.co.kr/bg/bg-main.webp',
+        width: 1200,
+        height: 630,
+        alt: 'OpenGraph 대표 이미지',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '용문산 허브 찜질방',
+    description:
+      '자연 속에 있는 찜질방,\n' +
+      '허브스팀으로 향기로운 찜질을 경험을 해보세요.',
+    images: ['https://herbsauna.co.kr/bg/bg-main.webp'],
+  },
 };
 
 export default function RootLayout({
